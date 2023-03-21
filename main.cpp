a55c99310a6aee99e77015d3eb705e084ffaf52f
Fionn O'Gorman <fionn3892@gmail.com>
2023-03-21 14:25:45 +0000

    Created a Pixmap and a Label and displayed image on screen

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QLabel>
+#include <QPixmap>
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+
+    //creating a pixmap and adding my image as a parameter
+    QPixmap pixmap("C:\\Users\\fionn\\OneDrive\\Desktop\\CookingAppMarkII\\recipe_rendezvous.png");
+
+    //creating a label and displaying the pixmap in the window using the setPixmap() function
+    QLabel label;
+    label.setPixmap(pixmap);
+
+    //resizing the window to the size of the pixmap
+    label.resize(pixmap.size());
+
+    //the function that displays the label on the screen
+    label.show();
+
+
     return a.exec();
 }
